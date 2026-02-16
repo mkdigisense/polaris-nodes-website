25c97c44fdd9c0681606cb30824992c38194bf7f
dappnode <dappnode@dappnode.digisense>
2026-02-16 15:16:13 +0200

    fix Get In Touch button on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -68,7 +68,7 @@ const Hero = () => {
       </div>
 
       {/* Bottom Gradient Fade */}
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10" />
+      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-0 pointer-events-none" />
     </section>
   );
 };
